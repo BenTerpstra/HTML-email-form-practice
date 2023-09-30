772e5a1e02ba2664cd26f0f4702b16e1049724b8
Ben Terpstra <bdterpstra@gmail.com>
2023-09-30 11:24:34 -0500

    Forgot to save before previous commit, adding updated file w/ pseudocode

--- HTML-email-form-practice.js
+++ HTML-email-form-practice.js
@@ -1,2 +1,19 @@
 //Lorem Ipsum//
 
+/// Contact info fieldset ///
+    // Form for name // **required
+
+    // Form for email // **required
+
+    // Form for phone number // **optional
+
+    // Form for location // **optional
+
+
+/// Message fieldset ///
+    // Radio buttons preferred method for response // **optional, default 'general'
+
+    // Dropdown menu for type of (or reason for) message // **optional, default email
+
+    // Form for message // **required
+
